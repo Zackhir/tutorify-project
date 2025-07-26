8a5767156d6ef49a8b6960fee899bb4c6c46c500
Zackhir <qurzakirbdu@gmail.com>
2025-07-26 20:59:32 +0400

    +Home section1 css

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,18 +5,18 @@ import icon1 from '../assets/icon1.svg'
 
 function Home() {
   return (
-    <div>Home
+    <div>
       <section className='section1'>
-          <div>
+          <div className='container1'>
             <div className='graphic-box'>
               <p>We are The Best </p>
               <div className='orange-line'></div>
             </div>
-            <h1>Learn Frome Home With The Best Online Languange Tutors</h1>
-            <p></p>
+            <h1>Learn From Home With <div className='orange'>The Best</div> Online Languange Tutors</h1>
+            <p>Start speaking confidently with top tutors. Try free lessons today and see the difference:</p>
             <Link>Try Free Lessons</Link>
           </div>
-          <div>
+          <div className='icon1Div'>
             <img src={icon1} className='icon1' alt='Component1'/>
           </div>
       </section>
