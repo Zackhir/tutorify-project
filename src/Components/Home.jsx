96bd734716d88c772e8fd92ccd4be048c2380aa6
Zackhir <qurzakirbdu@gmail.com>
2025-07-28 20:59:07 +0400

    +home section2

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './Home.css'
 import icon1 from '../assets/icon1.svg'
+import flag0 from '../assets/flag0.svg'
+import flag1 from '../assets/flag1.svg'
+import flag2 from '../assets/flag2.svg'
+import flag3 from '../assets/flag3.svg'
+import flag4 from '../assets/flag4.svg'
+import flag5 from '../assets/flag5.svg'
+import flag6 from '../assets/flag6.svg'
+import arrow from '../assets/arrow.svg'
+import icon2 from '../assets/icon2.svg'
+
 
 function Home() {
   return (
@@ -12,7 +22,7 @@ function Home() {
               <p>We are The Best </p>
               <div className='orange-line'></div>
             </div>
-            <h1>Learn From Home With <div className='orange'>The Best</div> Online Languange Tutors</h1>
+            <h1>Learn From Home With <span className='orange'>The Best</span> Online Languange Tutors</h1>
             <p>Start speaking confidently with top tutors. Try free lessons today and see the difference:</p>
             <Link>Try Free Lessons</Link>
           </div>
@@ -21,11 +31,43 @@ function Home() {
           </div>
       </section>
 
-      <section>
-
+      <section className='section2'>
+            <div className='container2'>
+              <p>What do you want to learn ?</p>
+                <h2>What We Offer</h2>
+                <div>
+                  <p>I want to learn </p>
+                  <div className='orange-line'></div>
+                </div>
+            </div>
+            <div className='container2-right'>
+                <p>We offer a wide variety of language lessons in many different languages, designed for all levels to support your learning and communication goals.</p>
+                <div>
+                  <div>
+                    <img src={flag0} width={64} height={64} alt="france" />
+                    <img src={flag1} width={64} height={64} alt="china" />
+                    <img src={flag2} width={64} height={64} alt="germany" />
+                    <img src={flag3} width={64} height={64} alt="japan" />
+                    <img src={flag4} width={64} height={64} alt="russia" />
+                    <img src={flag5} width={64} height={64} alt="mexico" />
+                    <img src={flag6} width={64} height={64} alt="spain" />
+                    <Link>View All 
+                    <img src={arrow} alt="" />  
+                    </Link>
+                    </div>
+                </div>
+            </div>
       </section>
-      <section>
-
+      <section className='section3'>
+            <div>
+              <h3></h3>
+              <p></p>
+              <div>
+                <Link></Link>
+                <Link></Link>
+              </div>
+            </div>
+            <div></div>
       </section>
       <section>
 
